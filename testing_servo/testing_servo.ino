0f725e16822d0b93fd2dc6bc8ded70919dd8bdfd
emilypondaven <emily.pondaven@gmail.com>
2026-06-16 23:02:58 +0100

    Further changes to check moving steering wheels

--- testing_servo/testing_servo.ino
+++ testing_servo/testing_servo.ino
@@ -6,21 +6,21 @@ int pos = 0;
 
 void setup() {
   Serial.begin(9600);
-  myservo.attach(2);
+  myservo.attach(8);
 
 }
 
 void loop() {
   Serial.println("Moving forward");
   // goes from 10 degrees to 170 degrees
-  for (pos = 10; pos < 170; pos += 1) {
+  for (pos = 1; pos < 50; pos += 1) {
     myservo.write(pos);
     delay(15);
   }
 
   Serial.println("Moving backwards");
   // goes from 180 degrees to 0 degrees
-  for (pos = 180; pos >= 1; pos-=1) {
+  for (pos = 50; pos >= 1; pos-=1) {
     myservo.write(pos);
     delay(15);
   }
